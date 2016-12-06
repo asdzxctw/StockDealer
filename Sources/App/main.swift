0534774b1d0ff061b330c18c9c2e6eca64d9cb52
asdzxctw <asdzxcasdzxctw@yahoo.com.tw>
2016-12-06 20:13:50 +0800

    取消查現在

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -25,22 +25,6 @@ drop.get("imageIndex") { request in
     return try drop.view.make("imageView.html")
 }
 
-drop.post("upload") { request in
-    
-    if let numStr = request.data["key"]?.string {
-        if let num = Int(numStr) {
-            if num>1000&&num<10000{
-                let pGetter = StockPriceGetter(number:Int(numStr)!)
-                
-                
-                return try JSON(node: pGetter.getPriceNow())
-            }
-        }
-        
-    }
-    return "Error retrieving parameters."
-}
-
 drop.post("hisPrice") { request in
     
     if let numStr = request.data["key"]?.string {
@@ -60,20 +44,6 @@ drop.post("hisPrice") { request in
     return "Error retrieving parameters."
 }
 
-drop.get("/stockNum",":key") { request in
-    if let numStr = request.parameters["key"]?.string {
-        if let num = Int(numStr) {
-            if num>1000&&num<10000{
-                let pGetter = StockPriceGetter(number:Int(numStr)!)
-                
-                return try JSON(node: pGetter.getPriceNow())
-            }
-        }
-        
-    }
-    return "Error retrieving parameters."
-    
-}
 
 drop.resource("posts", PostController())
 
